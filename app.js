7a7de0b0793f8a3e56f6230e0f3197b4eb39afa6
Danny Simms <djfsimms@hotmail.com>
2023-09-04 16:40:30 -0230

    Add attack dice select to character sheet
    Add a select field to track characters initial number of attack dice.
    Made function addOptions to add multiple options to the select field
    to avoid repeatedly manually adding each option.

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ addCharacterButton.addEventListener('click', () => {
 const newCharacterSheet = () => {
     const characterSheetDiv = document.createElement('div');
     characterSheetDiv.setAttribute('class', 'character-sheet');
-    
+
+    //Character name input field
     const characterNameDiv = document.createElement('div');
     const characterNameLabel = document.createElement('label');
     characterNameLabel.setAttribute('for', 'character-name');
@@ -16,6 +17,7 @@ const newCharacterSheet = () => {
     setAttributes(characterNameInput, {'type': 'text', 'id': 'character-name', 'name': 'character-name', 'required': ''});
     characterNameDiv.append(characterNameLabel, characterNameInput);
     
+    //Character type input field
     const characterTypeDiv = document.createElement('div');
     const characterTypeLabel = document.createElement('label');
     characterTypeLabel.setAttribute('for', 'character-type');
@@ -23,8 +25,26 @@ const newCharacterSheet = () => {
     const characterTypeInput = document.createElement('input');
     setAttributes(characterTypeInput, {'type': 'text', 'id': 'character-type', 'name': 'character-type', 'required': ''});
     characterTypeDiv.append(characterTypeLabel, characterTypeInput);
+
+    //Initial attack dice, defend dice and starting body and mind points
+    const initialStatsDiv = document.createElement('div');
+    initialStatsDiv.setAttribute('class', 'initial-stats');
+
+    //Attack dice select
+    const attackDiceDiv = document.createElement('div');
+    const attackDiceP = document.createElement('p');
+    attackDiceP.append('Attack Dice');
+    attackDiceDiv.append(attackDiceP);
+    const attackDiceSelect = document.createElement('select');
+    setAttributes(attackDiceSelect, {'id': 'attack-dice', 'class': 'numeric-tracker'});
+    const attackDiceOption = document.createElement('option');
+    addOptions(attackDiceSelect, 6);
+
+    attackDiceDiv.append(attackDiceP, attackDiceSelect)
+
+    initialStatsDiv.append(attackDiceDiv);
     
-    characterSheetDiv.append(characterNameDiv, characterTypeDiv);
+    characterSheetDiv.append(characterNameDiv, characterTypeDiv, initialStatsDiv);
 
     const createCharacterSheetDiv = document.getElementById('create-character-sheet');
     createCharacterSheetDiv.before(characterSheetDiv);
@@ -33,5 +53,15 @@ const newCharacterSheet = () => {
 function setAttributes(element, attributes) {
     for (let key in attributes) {
         element.setAttribute(key, attributes[key]);
+    }
+}
+
+function addOptions(element, numOfOptions) {
+    for (let i = 0; i <= numOfOptions; i++) {
+        const option = document.createElement('option');
+        option.setAttribute('value', i);
+        option.append(`${i}`);
+        element.appendChild(option);
+        console.log(option);
     }
 }
