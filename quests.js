2c46aa8ad4d617b6eef3f2d352583ed1c2b9c8ad
Danny Simms <djfsimms@hotmail.com>
2023-10-01 12:42:39 -0230

    Update quest html layout & quest title generation
    Changed the html layout of quest sheet attributes and updated some
    attribute names to clarify purposes and make code easier to understand.
    Quest title has been added to dynamcially generated quest sheet.

--- quests.js
+++ quests.js
@@ -1,10 +1,21 @@
-const createQuest = (questNum) => {
+const createQuest = (questNum, questName) => {
     const questsContainer = document.getElementById('quests-container');
 
     const questSheet = document.createElement('div');
-    setAttributes(questSheet, {'id': `quest-sheet-${questNum}`, 'class': 'quest-sheet'});
+    setAttributes(questSheet, {'class': 'quest-sheet', 'id': `quest-${questNum}-sheet`});
     questsContainer.append(questSheet);
-    console.log('createQuest working');
+
+    const questTitle = document.createElement('div');
+    setAttributes(questTitle, {'class': 'quest-title', 'id':`quest-${questNum}-title`});
+    questSheet.append(questTitle);
+
+    const questTitleNum = document.createElement('h3');
+    questTitleNum.append(`Quest ${questNum}`);
+
+    const questTitleName = document.createElement('h4');
+    questTitleName.append(`${questName}`);
+
+    questTitle.append(questTitleNum, questTitleName);
 }
 
-const nextQuest = createQuest(4);
+const nextQuest = createQuest(1, 'The Trial');
